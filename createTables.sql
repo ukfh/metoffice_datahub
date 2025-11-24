c282c60d044db88381d3b6376e96068f067bb361
uh <uliharder@gmail.com>
2025-11-24 21:07:00 +0000

    add table for old data

--- createTables.sql
+++ createTables.sql
@@ -30,3 +30,23 @@ create table if not exists observations
     obs_key           varchar(100)
 )
 ;
+
+drop table if exists staging.weather_processed_t;
+CREATE TABLE staging.weather_processed_t
+(
+    i         varchar(10),
+    name      varchar(100),
+    lat       double,
+    lon       double,
+    country   varchar(100),
+    continent varchar(100),
+    elevation varchar(10),
+    daykey    varchar(10),
+    hourkey   varchar(10),
+    metric    varchar(10),
+    value     varchar(10),
+    year      integer,
+    month     varchar(10),
+    day       varchar(10),
+    dt        varchar(10)
+);
