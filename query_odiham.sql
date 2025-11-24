2c1cd1a5ea0c27e0c2cb4f10f02642223c068be1
uh <uliharder@gmail.com>
2025-11-24 20:36:11 +0000

    reformat odiham query

--- query_odiham.sql
+++ query_odiham.sql
@@ -1,13 +1,12 @@
 SELECT dt,
-name,
-              metric,
-hourkey,
-concat(date_time, ' UTC') 		            date_time,
-
-ifnull(value,0)			           value
-				   FROM staging.observations_long
-				   where 1 = 1
-				   and name = 'gcp76x'
-				   and metric = 't'
-				   order by date_time desc
-				   ;
+       name,
+       metric,
+       hourkey,
+       concat(date_time, ' UTC') date_time,
+       ifnull(value, 0) value
+FROM staging.observations_long
+where 1 = 1
+  and name = 'gcp76x'
+  and metric = 't'
+order by date_time desc
+;
