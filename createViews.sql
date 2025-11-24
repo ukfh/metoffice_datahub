3ca37cd9b180ddcf6c967ea3ab1c21a8409c0263
Uli Harder <github@harder.me.uk>
2025-11-24 20:09:41 +0000

    update views script

--- createViews.sql
+++ createViews.sql
@@ -53,3 +53,69 @@ select report_date                               report_date_str,
        concat(datetime, '-', station_geohash)    obs_key
 from input_for_table
 ;
+
+
+create or replace view observations_long
+AS
+with observations_long
+         as (select cast(obs_datetime as date) dt
+                  , station_geohash            name
+                  , 'v'                        metric
+                  , hour(obs_datetime)         hourkey
+                  , obs_datetime               date_time
+                  , visibility                 value
+             from staging.observations
+             where 1 = 1
+               and station_geohash = 'gcp76x'
+
+             union all
+             select cast(obs_datetime as date) dt
+                  , station_geohash            name
+                  , 't'                        metric
+                  , hour(obs_datetime)         hourkey
+                  , obs_datetime               date_time
+                  , temperature                value
+             from staging.observations
+             where 1 = 1
+               and station_geohash = 'gcp76x'
+             union all
+             select cast(obs_datetime as date) dt
+                  , station_geohash            name
+                  , 'h'                        metric
+                  , hour(obs_datetime)         hourkey
+                  , obs_datetime               date_time
+                  , humidity                   value
+             from staging.observations
+
+             union all
+
+             select cast(obs_datetime as date) dt
+                  , station_geohash            name
+                  , 'p'                        metric
+                  , hour(obs_datetime)         hourkey
+                  , obs_datetime               date_time
+                  , mslp                       value
+             from staging.observations
+
+             union all
+
+             select cast(obs_datetime as date) dt
+                  , station_geohash            name
+                  , 'w'                        metric
+                  , hour(obs_datetime)         hourkey
+                  , obs_datetime               date_time
+                  , wind_speed                 value
+             from staging.observations
+
+             union all
+
+             select cast(obs_datetime as date) dt
+                  , station_geohash            name
+                  , 'dp'                       metric
+                  , hour(obs_datetime)         hourkey
+                  , obs_datetime               date_time
+                  , null                       value
+             from staging.observations)
+select dt, name, metric, hourkey, date_time, value
+from observations_long
+;
