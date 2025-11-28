0e1464df53b4de5c2cacb29faeed338c86aaabdb
uh <uliharder@gmail.com>
2025-11-28 20:58:12 +0000

    adding humidity

--- query_odiham.sql
+++ query_odiham.sql
@@ -7,7 +7,7 @@ SELECT dt,
 FROM staging.observations_long
 where 1 = 1
   and name = 'gcp76x'
-  and metric = 't'
+  and metric in ('t','h')
   and dt >= cast(SYSDATE() as date) - 8
 order by date_time desc
 ;
