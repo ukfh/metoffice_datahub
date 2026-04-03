aa29a3115ff0cb5f82d0973e054bf79e0d9307e4
uh <uli@harder.me.uk>
2026-04-03 15:39:27 +0100

    Update Odiham query

--- query_odiham.sql
+++ query_odiham.sql
@@ -4,7 +4,7 @@ SELECT dt,
        hourkey,
        concat(date_time, ' UTC') date_time,
        ifnull(value, 0)          value
-FROM staging.observations_long
+FROM processed.observations_long
 where 1 = 1
   and name = 'gcp76x'
   and metric in ('v', 't', 'h', 'p', 'w')
